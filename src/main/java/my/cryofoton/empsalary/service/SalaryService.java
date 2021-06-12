691afd9f3319889c6b7e80eb225fc4681b561913
Osman Sulaiman <sb.osman@outlook.my>
2021-06-13 00:23:04 +0800

    Add tax payable calculation

--- src/main/java/my/cryofoton/empsalary/service/SalaryService.java
+++ src/main/java/my/cryofoton/empsalary/service/SalaryService.java
@@ -3,7 +3,10 @@
  */
 package my.cryofoton.empsalary.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -31,23 +34,87 @@ public class SalaryService {
 		
 		//TODO throw exception if employee name not found in db
 		return employeesRepo.findById(name)
-				.map(e -> new EmployeeDto(e.getName(), 
-						calcYearlySalary(e.getMonthlySalary()), null))
+				.map(e -> {
+					BigDecimal yrSalary = calcYearlySalary(e.getMonthlySalary());
+					return new EmployeeDto(e.getName(), 
+							convertToSen(yrSalary), 
+							convertToSen(calcTaxPayable(yrSalary)));
+				})
 				.orElse(null);
 	}
 	
-	/**
-	 * This method calculates yearly salary from monthly salary.
-	 * 
-	 * @param monthly salary in RM
-	 * @return yearly salary in sen
-	 */
-	private Integer calcYearlySalary(Integer salary) {
-		if (salary == null) {
+	private BigDecimal calcYearlySalary(Integer salary) {
+		return Optional.ofNullable(salary)
+				.map(s -> s * 12)
+				.map(y -> new BigDecimal(y))
+				.orElse(null);
+	}
+	
+	private BigDecimal calcTaxPayable(BigDecimal yrSalary) {
+		if (yrSalary == null) {
 			return null;
 		}
 		
-		return salary * 12 * 100;
+		if (yrSalary.compareTo(BigDecimal.valueOf(5001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(20000)) <= 0) {
+			return calcTaxPayable(yrSalary, 5000, 0, 1.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(20001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(35000)) <= 0) {
+			return calcTaxPayable(yrSalary, 20000, 150, 3.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(35001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(50000)) <= 0) {
+			return calcTaxPayable(yrSalary, 35000, 600, 8.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(50001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(70000)) <= 0) {
+			return calcTaxPayable(yrSalary, 50000, 1800, 14.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(70001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(100000)) <= 0) {
+			return calcTaxPayable(yrSalary, 70000, 4600, 21.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(100001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(250000)) <= 0) {
+			return calcTaxPayable(yrSalary, 100000, 10900, 24.0);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(250001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(400000)) <= 0) {
+			return calcTaxPayable(yrSalary, 250000, 46900, 24.5);
+			
+		} else if (yrSalary.compareTo(BigDecimal.valueOf(400001)) >= 0 
+				&& yrSalary.compareTo(BigDecimal.valueOf(600000)) <= 0) {
+			return calcTaxPayable(yrSalary, 400000, 83650, 25.0);
+		}
+		
+		return BigDecimal.ZERO;
+	}
+	
+	private BigDecimal calcTaxPayable(BigDecimal yrSalary, Integer firstAmt, 
+			Integer firstTaxAmt, Double taxRate) {
+		
+		BigDecimal balance = new BigDecimal(yrSalary.toString());
+		BigDecimal tax = BigDecimal.ZERO;
+		
+		// process for first tier
+		balance = balance.subtract(BigDecimal.valueOf(firstAmt));
+		tax = tax.add(BigDecimal.valueOf(firstTaxAmt));
+		
+		// process for next tier
+		BigDecimal nextTax = balance
+				.multiply(BigDecimal.valueOf(taxRate))
+				.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+		tax = tax.add(nextTax);
+		
+		return tax;
+	}
+	
+	private Integer convertToSen(BigDecimal value) {
+		return Optional.ofNullable(value)
+				.map(v -> v.multiply(BigDecimal.valueOf(100)))
+				.map(v -> v.intValue())
+				.orElse(null);
 	}
 	
 	public EmployeeDto updateEmployeeSalary(EmployeeDto emp) {
