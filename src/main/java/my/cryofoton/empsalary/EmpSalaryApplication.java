c598f583af9dee220fabf444b79e2e4ef194841a
Osman Sulaiman <sb.osman@outlook.my>
2021-06-12 14:30:06 +0800

    Initial commit

---
+++ src/main/java/my/cryofoton/empsalary/EmpSalaryApplication.java
@@ -0,0 +1,13 @@
+package my.cryofoton.empsalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSalaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/cryofoton/empsalary/EmpSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package my.cryofoton.empsalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
